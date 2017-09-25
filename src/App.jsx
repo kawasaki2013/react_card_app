ab67bca3a7682a1ef90da14b7d1ecab1598541e5
ryotsuchiya <ryo_tsuchiya@mail.tmcc.edu>
2017-09-25 11:31:19 +0900

    second

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <h3>subtitle</h3>
+        <img src="https://facebook.github.io/react/img/logo_og.png" />
+      </div>
+    );
+  }
+}
+export default App;
